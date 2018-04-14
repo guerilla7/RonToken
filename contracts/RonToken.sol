dbc6e3eb85ad6efd1645a76fa774e6c765021deb
Ron F. del Rosario <ronsurf23@gmail.com>
2018-04-14 00:27:57 -0700

    RonToken.sol

---
+++ contracts/RonToken.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.17;
+
+import 'zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
+
+contract RonToken is StandardToken {
+  string public name = 'RonToken';
+  string public symbol = 'RT';
+  uint public decimals = 2;
+  uint public INITIAL_SUPPLY = 12000;
+
+  function RonToken() public {
+    
+  }
+}
