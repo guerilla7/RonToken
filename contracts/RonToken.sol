3d1cb85031c9890ef98b6f2668a626e4acde2824
Ron F. del Rosario <ronsurf23@gmail.com>
2018-04-14 11:06:13 -0700

    3 security fix applied using Solium

--- contracts/RonToken.sol
+++ contracts/RonToken.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.4.17;
 
-import 'zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
+import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
 contract RonToken is StandardToken {
-  string public name = 'RonToken';
-  string public symbol = 'RT';
+  string public name = "RonToken";
+  string public symbol = "RT";
   uint public decimals = 2;
   uint public INITIAL_SUPPLY = 12000;
 
